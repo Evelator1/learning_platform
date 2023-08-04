62223124a6e1c09f3c9b9c70d3fd10a9ebca7304
Omar-WD <129740652+Omar-WD@users.noreply.github.com>
2023-08-04 18:46:44 +0200

    Update JobAdDisplay.jsx

--- client/src/components/dashboard/tabs/JobSearch/JobAdDisplay.jsx
+++ client/src/components/dashboard/tabs/JobSearch/JobAdDisplay.jsx
@@ -137,6 +137,7 @@ export default function JobAdDisplay() {
           backgroundColor: cols.lila,
           border: `2px solid ${cols.gray}`,
           borderRadius: "0.5rem",
+          marginTop:"50px"
         }}
       >
         <Container>
